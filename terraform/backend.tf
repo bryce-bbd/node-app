b1207fc0a7266ace626848ccae883d55ad6f192b
Bryce <bryce.grahn@bbd.co.za>
2024-04-24 21:43:57 +0200

    trial run

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 # State bucket for storing and sharing terraform state
-# terraform {
-#   backend "s3" {
-#     bucket  = "${var.project_name}-bucket-1465"
-#     key     = "terraform-state/terraform.tfstate"
-#     region  = var.region
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "javascript-levelup-test-bucket-1465"
+    key     = "terraform-state/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
